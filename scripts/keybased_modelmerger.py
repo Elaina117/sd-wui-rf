678147c9fc540fd41195c1e688b9e25ea215212f
Elaina117 <hamyuu.2@gmail.com>
2024-11-30 08:10:14 +0900

    Update keybased_modelmerger.py

--- scripts/keybased_modelmerger.py
+++ scripts/keybased_modelmerger.py
@@ -36,8 +36,8 @@ class KeyBasedModelMerger(scripts.Script):
             return p
 
         try:
-            model_a_filename = sd_models.checkpoints_list[model_a_name].filename
-            model_b_filename = sd_models.checkpoints_list[model_b_name].filename
+            model_a_filename = model_a_name if os.path.isfile(model_a_name) else None
+            model_b_filename = model_b_name if os.path.isfile(model_b_name) else None
         except KeyError as e:
             print(f"Error: Selected model is not found in checkpoints list. {e}")
             return p
